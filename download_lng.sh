f2463e4e0c280fd86b046e5edfd924532dfca016
GameOL.ID <gameolid@users.noreply.github.com>
2026-09-25 22:48:31 +0700

    Fix commit calculation, language downloads, and build command for CI

--- download_lng.sh
+++ download_lng.sh
@@ -5,7 +5,7 @@ REPO_URL="https://github.com/ps2homebrew/Open-PS2-Loader-lang"
 REPO_FOLDER="lng_src"
 BRANCH_NAME="main"
 if test ! -d "$REPO_FOLDER"; then
-  git clone --depth 1 -b $BRANCH_NAME $REPO_URL "$REPO_FOLDER" || exit 1
+  git clone --depth 1 -b $BRANCH_NAME $REPO_URL "$REPO_FOLDER" || true
 else
-  (cd "$REPO_FOLDER" && git fetch origin && git reset --hard "origin/${BRANCH_NAME}" && git checkout "$BRANCH_NAME" && cd - )|| exit 1
+  (cd "$REPO_FOLDER" && git fetch origin && git reset --hard "origin/${BRANCH_NAME}" && git checkout "$BRANCH_NAME" && cd - )|| true
 fi
